b1e7a973d5cc546a980934e85874458fd2aaa7f7
Ris Adams <chris@tuesdaybegins.com>
2019-01-28 09:49:27 -0500

    Moving static tests to seperate file

---
+++ src/Naticon.Tests/MemberDataSerializer.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using Xunit.Abstractions;
+
+namespace Naticon.Tests
+{
+	public class MemberDataSerializer<T> : IXunitSerializable
+	{
+		public MemberDataSerializer()
+		{
+		}
+
+		public MemberDataSerializer(T objectToSerialize) => Object = objectToSerialize;
+
+		public T Object { get; private set; }
+
+		public void Deserialize(IXunitSerializationInfo info)
+		{
+			Object = JsonConvert.DeserializeObject<T>(info.GetValue<string>("objValue"));
+		}
+
+		public void Serialize(IXunitSerializationInfo info)
+		{
+			var json = JsonConvert.SerializeObject(Object);
+			info.AddValue("objValue", json);
+		}
+	}
+}
---
+++ src/Naticon.Tests/TestObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Naticon.Tests
+{
+	public class TestObject
+	{
+		public DateTime EndDate { get; set; }
+		public DateTime StartDate { get; set; }
+		public string Time1 { get; set; }
+		public string Time2 { get; set; }
+		public string Time3 { get; set; }
+		public string Time4 { get; set; }
+		public string Time5 { get; set; }
+		public string Time6 { get; set; }
+	}
+}
